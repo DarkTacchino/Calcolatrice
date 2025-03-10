4adf069daebb7080131023954393ab6ed80909c0
prova <st10517@iismarconipieralisi.it>
2025-03-10 16:20:36 +0100

    Versione 0.66

--- app/src/main/java/com/example/calcolatrice/MainActivity.java
+++ app/src/main/java/com/example/calcolatrice/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calcolatrice;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void backspaceBTN(View view) {
+
+    }
+    public void plusMinusBTN(View view) {
+
+    }
+    public void clearBTN(View view) {
+
+    }
+    public void parenthesesBTN(View view) {
+
+    }
+    public void pointBTN(View view) {
+
+    }
+
+    public void exponentBTN(View view) {
+
+    }
+
+    public void equalsBTN(View view) {
+
+    }
+
+    public void subtractBTN(View view) {
+
+    }
+
+    public void multiplyBTN(View view) {
+
+    }
+    public void addBTN(View view) {
+
+    }
+    public void divideBTN(View view) {
+
+    }
+
+
+
+
+
+    public void nineBTN(View view) {
+
+    }
+    public void eightBTN(View view) {
+
+    }
+    public void sevenBTN(View view) {
+
+    }
+    public void sixBTN(View view) {
+
+    }
+    public void fiveBTN(View view) {
+
+    }
+    public void fourBTN(View view) {
+
+    }
+    public void threeBTN(View view) {
+
+    }
+    public void twoBTN(View view) {
+
+    }
+    public void oneBTN(View view) {
+
+    }
+    public void zeroBTN(View view) {
+
     }
 }
